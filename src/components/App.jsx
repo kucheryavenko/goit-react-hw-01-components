2d67f34a8785626e7049254d4fbf6d52540b6ecb
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-09-06 18:15:27 +0300

    task-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
+import Profile from './Profile';
+import user from '../user.json';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        urlAvatar={user.avatar}  
+        userName={user.username} 
+        tag={user.tag} 
+        location={user.location} 
+        followers={user.stats.followers} 
+        views={user.stats.views} 
+        likes={user.stats.likes} 
+      />
     </div>
   );
-};
+}
